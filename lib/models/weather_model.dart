f52ae5d190c884c713a6c972f339797fb52b09ae
nuel232 <nwankwoala3@gmail.com>
2025-08-24 20:02:55 +0100

    Add weather functionality with API integration and UI updates
    - Implemented Weather model for API data handling
    - Created WeatherService for fetching weather data using OpenWeatherMap API
    - Added WeatherPage for displaying weather information
    - Integrated dotenv for environment variable management
    - Updated main.dart to load environment variables and set up the app
    - Added necessary dependencies in pubspec.yaml
    - Included .env file in .gitignore
    - Created generated plugin registrant files for Windows and Linux

---
+++ lib/models/weather_model.dart
@@ -0,0 +1,21 @@
+class Weather {
+  final String cityName;
+  final double temperature;
+  final String mainCondition;
+
+  Weather({
+    required this.cityName,
+    required this.temperature,
+    required this.mainCondition,
+  });
+
+  //method to deal with the json file
+  //this is to decode the information we get from the api(openWeatherMap)
+  factory Weather.fromJson(Map<String, dynamic> json) {
+    return Weather(
+      cityName: json['name'],
+      temperature: json['main']['temp'].toDouble(),
+      mainCondition: json['weather'][0]['main'],
+    );
+  }
+}
---
+++ lib/pages/weather_page.dart
@@ -0,0 +1,40 @@
+import 'package:dotenv/dotenv.dart';
+import 'package:flutter/material.dart';
+import 'package:weather_app/models/weather_model.dart';
+import 'package:weather_app/services/weather_service.dart';
+
+class WeatherPage extends StatefulWidget {
+  const WeatherPage({super.key});
+
+  @override
+  State<WeatherPage> createState() => _WeatherPageState();
+}
+
+class _WeatherPageState extends State<WeatherPage> {
+  //api key
+  final _WeatherService = WeatherService(apiKey: dotenv.env['API_KEY']!);
+  Weather? _weather;
+
+  //_FETCH WEATHER DATA
+  _fetchWeather() async {
+    //get the current city
+    String cityName = await _WeatherService.getCurrentCity();
+
+    //get weather for city
+    try {
+      final weather = await _WeatherService.getWeather(cityName);
+      setState(() {
+        _weather = weather;
+      });
+    }
+    //any error
+    catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
